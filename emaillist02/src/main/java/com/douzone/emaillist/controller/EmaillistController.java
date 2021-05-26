ac7705c056088eeaa4953e8e2c4e4e12b5ae8d85
jeonghyun051 <rlawjdgus051@naver.com>
2021-05-26 12:22:56 +0900

    emaillist02 분기 완료

--- emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -15,32 +15,49 @@ import com.douzone.emaillist.vo.EmaillistVo;
 public class EmaillistController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8"); // post 방식으로 전달받는 파라미터 값의 encoding 처리
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");	// post 방식으로 전달받는 파라미터 값의 encoding 처리
-		
 		String action = request.getParameter("a");
-	
-		if("form".equals(action)) {
-			System.out.println("form!");
-		} else if("add".equals(action)) {
-			System.out.println("add!");
+
+		if ("form".equals(action)) {
+			// 3. view로 포워딩
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
+		} else if ("add".equals(action)) {
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
+			
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+						
+			new EmaillistDao().insert(vo);
+			
+			// 2. redirect 응답
+			response.sendRedirect(request.getContextPath() + "/el");
+			
+			
 		} else {
 			/* default request(action) */
-			
+
 			// 1. 요청처리
 			List<EmaillistVo> list = new EmaillistDao().findAll();
-			
+
 			// 2. request범위에 데이터(객체) 저장
 			request.setAttribute("list", list);
-			
+
 			// 3. view로 포워딩
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
 			rd.forward(request, response);
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		doGet(request, response);
 	}
